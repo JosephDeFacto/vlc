aacde88526e4cd6ae260b67ec59a8cbae0d2574c
JosephDeFacto <josip.mareljic@gmail.com>
2023-10-08 13:09:34 +0200

    Created wrapper function for handling requests and responses.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="input-group">
         <input type="text" class="form-control name-input" name="q" placeholder="Search TV Shows">
         <div class="input-group-append">
-            <button class="btn btn-primary" type="submit">Search</button>
+            <button class="btn btn-primary" onclick="getResponse(event);">Search</button>
         </div>
     </div>
 </form>
@@ -12,9 +12,9 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
-    $(document).ready(function() {
-        $('.btn-primary').on('click', function (event) {
-            event.preventDefault();
+
+        let getResponse = (event) => {
+            event.preventDefault()
             let q = $('.name-input').val();
             let resultsId = $('#results');
 
@@ -33,7 +33,6 @@
                     let err = JSON.parse(xhr.responseText);
                     resultsId.append(err.error);
                 }
-            })
-        })
-    });
+            });
+        }
 </script>
